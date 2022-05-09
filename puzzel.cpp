ea31f3ccd140a6d7f18e1812e6902953aa9ea7f7
arpan-svci <arpanmandal913@gmail.com>
2022-05-10 01:11:02 +0530

    commit

--- puzzel.cpp
+++ puzzel.cpp
@@ -1,19 +1,21 @@
 #include<bits/stdc++.h>
+#include <unistd.h>
 using namespace std;
 
 int main(){
-cout<<"hello";
 vector<vector<int>>v{{0,-1},{0,1},{1,0},{-1,0}};
 vector<vector<vector<int>>>states;
 queue<vector<vector<vector<int>>>>q;
-vector<vector<int>>initial{{2,3,4},{1,8,7},{6,5,0}};
+vector<vector<int>>initial{{1,2,3},{8,4,5},{7,6,0}};
 vector<vector<int>>goal{{1,2,3},{4,5,6},{7,8,0}};
 vector<vector<vector<int>>>temp;
 temp.push_back(initial);
 q.push(temp);
+int count=1;
 while(!q.empty()){
-    cout<<"hello";
+    // sleep(2);
     temp=q.front();
+    q.pop();
     if(temp[temp.size()-1]==goal){
         int count=1;
         for(auto i:temp){
@@ -35,15 +37,25 @@ while(!q.empty()){
     }
     if(!present){
         states.push_back(temp[temp.size()-1]);
+        cout<<'\n'<<count++<<"state :"<<'\n';
+        for(auto i:temp[temp.size()-1]){
+            for(auto j:i){
+                cout<<j<<'\t';
+            }
+            cout<<endl;
+        }
+
         vector<vector<int>>state=temp[temp.size()-1];
+        vector<vector<int>>state1;
         for(int i=0;i<state.size();i++){
-            for(int j=0;j<state.size();j++){
+            for(int j=0;j<state[0].size();j++){
                 if(state[i][j]==0){
                     for(auto k:v){
-                        if(i+k[0]>=0 && j+k[1]<3){
-                            state[i][j]=state[i+k[0]][j+k[1]];
-                            state[i+k[0]][j+k[1]]=0;
-                            temp.push_back(state);
+                        state1=state;
+                        if(i+k[0]>=0 && i+k[0]<3 && j+k[1]>=0 && j+k[1]<3){
+                            state1[i][j]=state1[i+k[0]][j+k[1]];
+                            state1[i+k[0]][j+k[1]]=0;
+                            temp.push_back(state1);
                             q.push(temp);
                             temp.pop_back();
                         }
